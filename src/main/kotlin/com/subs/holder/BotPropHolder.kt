b52872ff784c0b96550820677ea0c4a3bb13d500
mrz_r <mrz_rom2@mail.ru>
2025-03-23 21:34:11 +0300

    COM-009 - рефакторинг

--- src/main/kotlin/com/subs/holder/BotPropHolder.kt
+++ src/main/kotlin/com/subs/holder/BotPropHolder.kt
@@ -5,7 +5,7 @@ import lombok.Getter
 @Getter
 class BotPropHolder {
 
-    val botToken: String = "7726090136:AAEckSntsrCC-voSsZFBwYiMAMcZyN8kIXE"
+    val botToken: String = ""
 
-    val botUsername: String = "ManageSubs001Bot"
+    val botUsername: String = ""
 }
